59299bd8c5a97d4a1fc46645180311058e4a6d3a
susan <susan@asus>
2018-09-12 10:58:59 +0700

    Initial Commit

---
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -0,0 +1,16 @@
+package DynamicProgramming;
+public class HitungFibonacci {
+    public static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("Hitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21,......dst");
+    }
+     public static void main(String args[])
+    {
+        String identitas = "Stefanus Ario Prabowo / X RPL 3/ 37";
+        
+        tampilJudul(identitas);
+    }
+}
