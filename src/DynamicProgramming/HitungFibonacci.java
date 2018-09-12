d029b222879e0772aa6071374d28e71150e3ad37
susan <susan@asus>
2018-09-12 11:11:01 +0700

    Tambah Input

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -1,4 +1,5 @@
 package DynamicProgramming;
+import java.util.Scanner;
 public class HitungFibonacci {
     public static void tampilJudul(String identitas)
     {
@@ -7,10 +8,22 @@ public class HitungFibonacci {
         System.out.println("Hitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21,......dst");
     }
+    public static int tampilInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke- ");
+        int n = scanner.nextInt();
+        
+        return n;     
+    }
      public static void main(String args[])
     {
         String identitas = "Stefanus Ario Prabowo / X RPL 3/ 37";
         
         tampilJudul(identitas);
+        
+        int n = tampilInput();
+
     }
 }
